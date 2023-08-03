e9497fcec5d3615b34de8ff966c3096a316128e6
Nnamanijames <nnamani911@gmail.com>
2023-08-03 13:49:31 +0100

    modified html amd js files

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const name = document.getElementById("name");
+const school = document.getElementById("school");
 
 name.onclick = function(){
     name.innerHTML="NVIT";
+}
+
+
+school.onclick = function(){
+    school.innerHTML = "I am taking Python course"
 }
